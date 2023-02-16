7569259a14f71c94745fb1e7772d8129171dd1a2
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-02-15 20:01:24 -0500

    React app skeleton

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <>
+      <main>
+        <section>
+          <h3>birthdays today</h3>
+          <button className='btn btn-primary'>clear all</button>
+        </section>
+      </main>
+    </>
+  )
+}
